0627bae79fdfeff4a23a0c1e8cd767bcb6db14e0
Lullett <lucrezia.vetro@studenti.univr.it>
2021-06-11 01:09:22 +0200

    voglio morire ciao

--- src/sample/Registrazione.java
+++ src/sample/Registrazione.java
@@ -1,18 +1,57 @@
 package sample;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
 import javafx.scene.control.Alert;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.*;
 import javax.swing.*;
 import java.io.IOException;
 
 public class Registrazione extends JFrame {
+    private double xOffset = 0;
+    private double yOffset = 0;
+    private static class Delta {
+        double x, y;
+    }
+
+    final Home.Delta dragDelta = new Home.Delta();
+    final BooleanProperty inDrag = new SimpleBooleanProperty(false);
 
     public Registrazione() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Registrazione.fxml"));
         Parent root1 = fxmlLoader.load();
         Stage stage = new Stage();
+
+        final double[] xOffset = {0};
+        final double[] yOffset = {0};
+
+        root1.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                dragDelta.x = stage.getX() - event.getScreenX();
+                dragDelta.y = stage.getY() - event.getScreenY();
+                xOffset[0] = event.getSceneX();
+                yOffset[0] = event.getSceneY();
+            }
+        });
+
+        root1.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() + dragDelta.x);
+                stage.setY(event.getScreenY() + dragDelta.y);
+                stage.getWidth();
+                stage.getHeight();
+                stage.getX();
+                stage.getY();
+                inDrag.set(true);
+
+            }
+        });
         stage.initModality(Modality.WINDOW_MODAL);
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setTitle("Home");
